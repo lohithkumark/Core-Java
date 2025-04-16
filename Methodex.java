c38edbc96435537b558d06191056c8ccfc5a5a5f
Lohith kumar K <lohitkumar2003@gmail.com>
2025-04-16 18:52:02 +0530

    Add files via upload

---
+++ Methodex.java
@@ -0,0 +1,14 @@
+public class Methodex {
+
+    public static int addNumbers(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = addNumbers(2,4);
+        System.out.println(result);
+
+    }
+}
+
