8056031a14de2f683957d23e5d00f7817670578f
Lohith kumar K <lohitkumar2003@gmail.com>
2025-04-16 18:57:39 +0530

    Add files via upload

---
+++ Methodss.java
@@ -0,0 +1,16 @@
+public class Methodss {
+
+    public static int hello(int a ,int b){
+        return a+b;
+    }
+
+    public static int hell(int a,int b){
+        return a+b;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(hell(23,45));
+        System.out.println(hello(54,65));
+    }
+}
